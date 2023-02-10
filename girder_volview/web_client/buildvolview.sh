1ce9cee66d1c9af4eed3e7d96d33e969ba513e4b
Paul Elliott <paul.elliott@kitware.com>
2023-02-10 13:03:06 -0500

    fix(buildvolview): patch volview dependencies before install
    Point at Kitware/VolView Github repo.

--- girder_volview/web_client/buildvolview.sh
+++ girder_volview/web_client/buildvolview.sh
@@ -1,8 +1,9 @@
 #!/bin/sh bash
 rm -rf VolView
-git clone --branch package https://github.com/PaulHax/VolView.git
+git clone https://github.com/Kitware/VolView.git
 
 cd VolView
 npm install
+npm run postinstall
 VUE_APP_PUBLIC_PATH=/static/built/plugins/volview npm run build
 
