4b57ab376f89aa39bcc6f89e35390cc8b069deae
Paul Elliott <paul.elliott@kitware.com>
2023-04-05 15:45:24 -0400

    refactor(itemPage): open VolView tab with function

--- girder_volview/web_client/views/itemPage.js
+++ girder_volview/web_client/views/itemPage.js
@@ -1,23 +1,18 @@
 import { wrap } from "@girder/core/utilities/PluginUtils";
 import ItemView from "@girder/core/views/body/ItemView";
-
-const origin = globalThis.location.origin;
-const volViewPath = `${origin}/static/built/plugins/volview/index.html`;
-const fileRoot = `${origin}/api/v1/item`;
+import { open } from "./utils";
 
 const brandName = "VolView";
 
 wrap(ItemView, "render", function (render) {
     this.once("g:rendered", function () {
         this.$el.find(".g-item-header .btn-group").before(
-            `<a class="btn btn-sm btn-primary" style="margin-left: 10px" role="button" 
-                href="${volViewPath}?names=[${this.model.name()}]&urls=[${fileRoot}/${
-                this.model.id
-            }/download]" target="_blank"
-            >
+            `<a class="btn btn-sm btn-primary open-in-volview" style="margin-left: 10px" role="button">
                 <i class="icon-link-ext"></i>Open in ${brandName}
             </a>`
         );
+        const buttons = this.$el.find(".open-in-volview");
+        buttons[0].onclick = () => open(this.model);
     });
     render.call(this);
 });
