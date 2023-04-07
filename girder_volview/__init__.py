5bbf0a23b6136c3fd73c8b33808d735cc8335d53
Paul Elliott <paul.elliott@kitware.com>
2023-04-06 22:35:42 -0400

    feat(girder_volview): add save route to item and pass save url parameter

--- girder_volview/__init__.py
+++ girder_volview/__init__.py
@@ -1,10 +1,22 @@
 from girder import plugin
 
+from girder.api.describe import Description, autoDescribeRoute
+from girder.api import access 
+from girder.api.v1.file import File 
+from girder.constants import AccessType, TokenScope
+
+@access.public(cookie=True, scope=TokenScope.DATA_WRITE)
+@autoDescribeRoute(
+    Description('Save VolView session with an item')
+    .param('id', 'The item ID', paramType='path')
+    .errorResponse())
+def saveVolView(id):
+    return File().initUpload('item', id, 'session.volview.zip', 1, 'application/zip')
 
 class GirderPlugin(plugin.GirderPlugin):
     DISPLAY_NAME = 'VolView'
     CLIENT_SOURCE_PATH = 'web_client'
 
     def load(self, info):
-        # add plugin loading logic here
-        pass
+        info['apiRoot'].item.route('POST', (':id', 'volview'), saveVolView)
+
