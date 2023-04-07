a3d285bb4f9c14481a9dfe90aec426dfd494b029
Paul Elliott <paul.elliott@kitware.com>
2023-04-06 22:35:42 -0400

    feat(girder_volview): add save route to item and pass save url parameter

--- girder_volview/web_client/views/utils.js
+++ girder_volview/web_client/views/utils.js
@@ -3,8 +3,9 @@ const volViewPath = `${origin}/static/built/plugins/volview/index.html`;
 const fileRoot = `${origin}/api/v1/item`;
 
 export function open(model) {
-    const downloadUrl = `${volViewPath}?names=[${model.name()}]&urls=[${fileRoot}/${
-        model.id
-    }/download]`;
-    window.open(downloadUrl, "_blank").focus();
+    const itemRoot = `${fileRoot}/${model.id}`;
+    const downloadUrl = `${itemRoot}/download`;
+    const saveUrl = `${itemRoot}/volview`;
+    const volViewUrl = `${volViewPath}?names=[${model.name()}]&urls=[${downloadUrl}]&save=${saveUrl}`;
+    window.open(volViewUrl, "_blank").focus();
 }
