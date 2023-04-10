6936d9b61ade18a78503153b4b0fa0ff6c7cb6f4
Paul Elliott <paul.elliott@kitware.com>
2023-04-10 18:30:04 -0400

    chore(buildvolview): enable remote saving for VolView client

--- girder_volview/web_client/buildvolview.sh
+++ girder_volview/web_client/buildvolview.sh
@@ -5,5 +5,5 @@ git clone https://github.com/Kitware/VolView.git
 cd VolView
 npm install
 npm run postinstall
-VUE_APP_PUBLIC_PATH=/static/built/plugins/volview npm run build
+VUE_APP_PUBLIC_PATH=/static/built/plugins/volview VUE_APP_ENABLE_REMOTE_SAVE=true npm run build
 
