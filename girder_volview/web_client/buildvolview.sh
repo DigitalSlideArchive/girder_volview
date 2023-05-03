c14c4b0a37c3089419084c7bec38424264578202
Paul Elliott <paul.elliott@kitware.com>
2023-05-03 14:40:14 -0400

    feat(buildvolview): pin volview client version

--- girder_volview/web_client/buildvolview.sh
+++ girder_volview/web_client/buildvolview.sh
@@ -1,8 +1,14 @@
 #!/bin/sh bash
 rm -rf VolView
-git clone https://github.com/Kitware/VolView.git
-
+mkdir VolView
 cd VolView
+
+# fetch just one commit
+git init
+git remote add origin https://github.com/Kitware/VolView.git
+git fetch origin 44437af3dd7bcf4e8f94fbab22c6f2c56f40d8d9 --depth 1
+git reset --hard FETCH_HEAD
+
 npm install
 npm run postinstall
 VUE_APP_PUBLIC_PATH=/static/built/plugins/volview VUE_APP_ENABLE_REMOTE_SAVE=true npm run build
