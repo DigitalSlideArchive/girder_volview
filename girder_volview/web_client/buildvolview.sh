5bd07295767175559ab40bd61f3d87c1f6f20289
Paul Elliott <paul.elliott@kitware.com>
2023-05-16 19:03:24 -0400

    chore(buildvolview): update VolView client sha close to v4.1

--- girder_volview/web_client/buildvolview.sh
+++ girder_volview/web_client/buildvolview.sh
@@ -6,7 +6,7 @@ cd VolView
 # fetch just one commit
 git init
 git remote add origin https://github.com/Kitware/VolView.git
-git fetch origin 44437af3dd7bcf4e8f94fbab22c6f2c56f40d8d9 --depth 1
+git fetch origin 44b05941bb85c77bf8fe5b037f7a9a4e8ce67ff2 --depth 1
 git reset --hard FETCH_HEAD
 
 npm install
