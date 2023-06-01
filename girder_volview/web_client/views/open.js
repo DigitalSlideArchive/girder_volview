000b5be5b83ffaea8114ec048b39990287779c52
Paul Elliott <paul.elliott@kitware.com>
2023-06-01 17:14:24 -0400

    feat(open): pass label config as remote file to load

--- girder_volview/web_client/views/open.js
+++ girder_volview/web_client/views/open.js
@@ -2,38 +2,25 @@ import { restRequest } from "@girder/core/rest";
 
 const origin = globalThis.location.origin;
 const volViewPath = `${origin}/static/built/plugins/volview/index.html`;
-const fileRoot = `${origin}/api/v1/item`;
+const apiRoot = `${origin}/api/v1`;
+const itemApi = `${apiRoot}/item`;
 
 function isSessionFile(fileName) {
     return fileName.endsWith("volview.zip");
 }
 
-function makeDownloadParams(model, itemRoot, files) {
+function makeDownloadParams(model, itemRoute, files) {
     if (files.length === 0) return "";
 
     const hasSessionFiles = files.some(({ name }) => isSessionFile(name));
     const downloadUrl = hasSessionFiles
-        ? `${itemRoot}/volview`
-        : `${itemRoot}/volview/datasets`;
+        ? `${itemRoute}/volview`
+        : `${itemRoute}/volview/datasets`;
 
-    return `&names=[${model.name()}.zip]&urls=[${downloadUrl}]`;
-}
+    const folderID = model.get("folderId");
+    const configUrl = `${apiRoot}/folder/${folderID}/yaml_config/.volview_config.yaml`;
 
-function makeLabelParam(labels) {
-    if (!labels) return "";
-
-    const labelsArray = Object.entries(labels)
-        .map(([name, props]) => [name, ...props])
-        .join(",");
-    return labelsArray;
-}
-
-function makeLabelParams(config) {
-    if (!config) return "";
-
-    return `&labels=[${makeLabelParam(
-        config.labels
-    )}]&rectangleLabels=[${makeLabelParam(config.rectangleLabels)}]`;
+    return `&names=[${model.name()}.zip,config.json]&urls=[${downloadUrl},${configUrl}]`;
 }
 
 export function open(model) {
@@ -43,31 +30,11 @@ export function open(model) {
         error: null,
     })
         .done((files) => {
-            restRequest({
-                url: `folder/${model.get(
-                    "folderId"
-                )}/yaml_config/.volview_config.yaml`,
-            })
-                .done((config) => {
-                    const itemRoot = `${fileRoot}/${model.id}`;
-                    const saveParam = `&save=${itemRoot}/volview`;
-                    const downloadParams = makeDownloadParams(
-                        model,
-                        itemRoot,
-                        files
-                    );
-                    const labelParams = makeLabelParams(config);
-                    const newTabUrl = `${volViewPath}?${labelParams}${saveParam}${downloadParams}`;
-                    window.open(newTabUrl, "_blank").focus();
-                })
-                .fail((resp) => {
-                    events.trigger("g:alert", {
-                        icon: "cancel",
-                        text: "Could not check files to open in VolView",
-                        type: "danger",
-                        timeout: 4000,
-                    });
-                });
+            const itemRoute = `${itemApi}/${model.id}`;
+            const saveParam = `&save=${itemRoute}/volview`;
+            const downloadParams = makeDownloadParams(model, itemRoute, files);
+            const newTabUrl = `${volViewPath}?${saveParam}${downloadParams}`;
+            window.open(newTabUrl, "_blank").focus();
         })
         .fail((resp) => {
             events.trigger("g:alert", {
