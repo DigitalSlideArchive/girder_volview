3466f69991a99280fdf44092f2836ccade1f5286
Paul Elliott <paul.elliott@kitware.com>
2023-06-13 15:25:23 -0400

    feat(client): bump client for labels on tab, axial view and fixes

--- volview-girder-client/buildvolview.sh
+++ volview-girder-client/buildvolview.sh
@@ -6,10 +6,11 @@ cd VolView
 # fetch just one commit
 git init
 git remote add origin https://github.com/Kitware/VolView.git
-git fetch origin f1f7a77c5d69f411300bb6afc4475d3d0ba03eb4 --depth 1
+git fetch origin 856a0d2387738c488b556084e797467586477d0e --depth 1
 git reset --hard FETCH_HEAD
 
 npm install
 npm run postinstall
 VUE_APP_PUBLIC_PATH=/static/built/plugins/volview VUE_APP_ENABLE_REMOTE_SAVE=true npm run build
 
+cd ..
