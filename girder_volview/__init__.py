900cf2e22c99721b82c59e0588d208ed2387d1b4
Paul Elliott <paul.elliott@kitware.com>
2023-08-03 13:48:45 -0400

    refactor(girder_volview): format with black

--- girder_volview/__init__.py
+++ girder_volview/__init__.py
@@ -4,7 +4,7 @@ import errno
 from girder import plugin
 
 from girder.api.describe import Description, autoDescribeRoute
-from girder.api import access 
+from girder.api import access
 from girder.api.rest import boundHandler, setResponseHeader, setContentDisposition
 from girder.constants import AccessType, TokenScope
 
@@ -19,24 +19,27 @@ from girder.exceptions import GirderException
 from girder.models.item import Item as ItemModel
 from girder.utility import ziputil
 
+
 @access.public(cookie=True, scope=TokenScope.DATA_WRITE)
 @boundHandler
 @autoDescribeRoute(
-    Description('Save VolView session in an item')
-    .param('itemId', 'The item ID', paramType='path')
-    .errorResponse())
+    Description("Save VolView session in an item")
+    .param("itemId", "The item ID", paramType="path")
+    .errorResponse()
+)
 def saveSession(self, itemId):
-    size = int(cherrypy.request.headers.get('Content-Length'))
+    size = int(cherrypy.request.headers.get("Content-Length"))
     if size == 0:
         raise GirderException(
-            'Expected non-zero Content-Length header', 'girder.api.v1.item.save-volview')
+            "Expected non-zero Content-Length header", "girder.api.v1.item.save-volview"
+        )
 
-    # modified from girder.api.v1.file.File.initUpload 
+    # modified from girder.api.v1.file.File.initUpload
     fileModel = FileModel()
     parentId = itemId
-    parentType = 'item'
-    name = 'session.volview.zip'
-    mimeType = 'application/zip'
+    parentType = "item"
+    name = "session.volview.zip"
+    mimeType = "application/zip"
     reference = None
     user = self.getCurrentUser()
     parent = Item().load(id=parentId, user=user, level=AccessType.WRITE, exc=True)
@@ -45,22 +48,33 @@ def saveSession(self, itemId):
 
     chunk = None
     ct = cherrypy.request.body.content_type.value
-    if (ct not in cherrypy.request.body.processors
-            and ct.split('/', 1)[0] not in cherrypy.request.body.processors):
+    if (
+        ct not in cherrypy.request.body.processors
+        and ct.split("/", 1)[0] not in cherrypy.request.body.processors
+    ):
         chunk = RequestBodyStream(cherrypy.request.body)
     if chunk is not None and chunk.getSize() <= 0:
         chunk = None
 
     try:
         upload = Upload().createUpload(
-            user=user, name=name, parentType=parentType, parent=parent, size=size,
-            mimeType=mimeType, reference=reference, assetstore=assetstore)
+            user=user,
+            name=name,
+            parentType=parentType,
+            parent=parent,
+            size=size,
+            mimeType=mimeType,
+            reference=reference,
+            assetstore=assetstore,
+        )
     except OSError as exc:
         if exc.errno == errno.EACCES:
             raise GirderException(
-                'Failed to create upload.', 'girder.api.v1.item.volview.create-upload-failed')
+                "Failed to create upload.",
+                "girder.api.v1.item.volview.create-upload-failed",
+            )
         raise
-    if upload['size'] > 0:
+    if upload["size"] > 0:
         if chunk:
             return Upload().handleChunk(upload, chunk, filter=True, user=user)
 
@@ -68,60 +82,78 @@ def saveSession(self, itemId):
     else:
         return fileModel.filter(Upload().finalizeUpload(upload), user)
 
+
 def isSessionFile(path):
-    if path.endswith('volview.zip'):
+    if path.endswith("volview.zip"):
         return True
     return False
 
+
 @access.public(cookie=True, scope=TokenScope.DATA_READ)
 @boundHandler
 @autoDescribeRoute(
-    Description('Download item files that do not end in volview.zip')
-    .modelParam('itemId', model=ItemModel, level=AccessType.READ)
-    .produces(['application/zip'])
-    .errorResponse('ID was invalid.')
-    .errorResponse('Read access was denied for the item.', 403))
+    Description("Download item files that do not end in volview.zip")
+    .modelParam("itemId", model=ItemModel, level=AccessType.READ)
+    .produces(["application/zip"])
+    .errorResponse("ID was invalid.")
+    .errorResponse("Read access was denied for the item.", 403)
+)
 def downloadDatasets(self, item):
-    setResponseHeader('Content-Type', 'application/zip')
-    setContentDisposition(item['name'] + '.zip')
+    setResponseHeader("Content-Type", "application/zip")
+    setContentDisposition(item["name"] + ".zip")
 
     def stream():
-        zip = ziputil.ZipGenerator(item['name'])
-        sansSessions = [ fileEntry for fileEntry in ItemModel().fileList(item, subpath=False) if not isSessionFile(fileEntry[0]) ]
-        for (path, file) in sansSessions:
+        zip = ziputil.ZipGenerator(item["name"])
+        sansSessions = [
+            fileEntry
+            for fileEntry in ItemModel().fileList(item, subpath=False)
+            if not isSessionFile(fileEntry[0])
+        ]
+        for path, file in sansSessions:
             for data in zip.addFile(file, path):
                 yield data
         yield zip.footer()
+
     return stream
+
 
 @access.public(cookie=True, scope=TokenScope.DATA_READ)
 @boundHandler
 @autoDescribeRoute(
-    Description('Download latest *.volview.zip')
-    .modelParam('itemId', model=ItemModel, level=AccessType.READ)
-    .produces(['application/zip'])
-    .errorResponse('ID was invalid.')
-    .errorResponse('Read access was denied for the item.', 403))
+    Description("Download latest *.volview.zip")
+    .modelParam("itemId", model=ItemModel, level=AccessType.READ)
+    .produces(["application/zip"])
+    .errorResponse("ID was invalid.")
+    .errorResponse("Read access was denied for the item.", 403)
+)
 def downloadSession(self, item):
-    setResponseHeader('Content-Type', 'application/zip')
-    setContentDisposition(item['name'] + '.zip')
+    setResponseHeader("Content-Type", "application/zip")
+    setContentDisposition(item["name"] + ".zip")
 
-    sessions = [ fileEntry[1] for fileEntry in ItemModel().fileList(item, subpath=False, data=False) if isSessionFile(fileEntry[0]) ]
+    sessions = [
+        fileEntry[1]
+        for fileEntry in ItemModel().fileList(item, subpath=False, data=False)
+        if isSessionFile(fileEntry[0])
+    ]
     if len(sessions) == 0:
         raise GirderException(
-            'No VolView session file found.', 'girder.api.v1.item.volview.download-session')
+            "No VolView session file found.",
+            "girder.api.v1.item.volview.download-session",
+        )
 
-    sortedSessions = sorted(sessions, key=lambda file: file.get('created'))
+    sortedSessions = sorted(sessions, key=lambda file: file.get("created"))
     latestSession = sortedSessions[-1]
 
     return FileModel().download(latestSession, 0)
 
+
 class GirderPlugin(plugin.GirderPlugin):
-    DISPLAY_NAME = 'VolView'
-    CLIENT_SOURCE_PATH = 'web_client'
+    DISPLAY_NAME = "VolView"
+    CLIENT_SOURCE_PATH = "web_client"
 
     def load(self, info):
-        info['apiRoot'].item.route('POST', (':itemId', 'volview'), saveSession)
-        info['apiRoot'].item.route('GET', (':itemId', 'volview'), downloadSession)
-        info['apiRoot'].item.route('GET', (':itemId', 'volview', 'datasets'), downloadDatasets)
-
+        info["apiRoot"].item.route("POST", (":itemId", "volview"), saveSession)
+        info["apiRoot"].item.route("GET", (":itemId", "volview"), downloadSession)
+        info["apiRoot"].item.route(
+            "GET", (":itemId", "volview", "datasets"), downloadDatasets
+        )
