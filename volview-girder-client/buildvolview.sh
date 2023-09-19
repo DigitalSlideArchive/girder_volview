bcca24cc4984998cb92c6d684533c3affeb8761e
Paul Elliott <paul.elliott@kitware.com>
2023-09-19 14:59:15 -0400

    feat(client): bump build for hover

--- volview-girder-client/buildvolview.sh
+++ volview-girder-client/buildvolview.sh
@@ -6,11 +6,11 @@ cd VolView
 # fetch just one commit
 git init
 git remote add origin https://github.com/Kitware/VolView.git
-git fetch origin 370cbce6811612cf940ab76d88d5a303669d3e1f --depth 1
+git fetch origin 9ff6990d31d2ff66084eb59913db7c0fea40dca3 --depth 1
 git reset --hard FETCH_HEAD
 
 npm install
 npm run postinstall # avoid starting the build before patch-package done by running postinstall manualy 
-VITE_ENABLE_REMOTE_SAVE=true npm run build -- --base=/static/built/plugins/volview
+ VITE_REMOTE_SERVER_URL= VITE_ENABLE_REMOTE_SAVE=true npm run build -- --base=/static/built/plugins/volview
 
 cd ..
