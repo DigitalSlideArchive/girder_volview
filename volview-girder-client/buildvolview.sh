9fd2b54fe55714611ae87ac4c9abd5122eeeca51
Paul Elliott <paul.elliott@kitware.com>
2023-10-05 17:16:44 -0400

    feat(volview-client): add count of tools and reset window level

--- volview-girder-client/buildvolview.sh
+++ volview-girder-client/buildvolview.sh
@@ -6,11 +6,14 @@ cd VolView
 # fetch just one commit
 git init
 git remote add origin https://github.com/Kitware/VolView.git
-git fetch origin 9ff6990d31d2ff66084eb59913db7c0fea40dca3 --depth 1
+git fetch origin a25452bb36912a14e1f02e43bc24673923954783 --depth 1
 git reset --hard FETCH_HEAD
 
 npm install
 npm run postinstall # avoid starting the build before patch-package done by running postinstall manualy 
  VITE_REMOTE_SERVER_URL= VITE_ENABLE_REMOTE_SAVE=true npm run build -- --base=/static/built/plugins/volview
+
+# remote so npm publish picks up VolView/dist
+rm .gitignore
 
 cd ..
