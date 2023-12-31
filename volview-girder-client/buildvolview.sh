2e57d6840f4c3508e7a9696be0dc8bd005909dab
Paul Elliott <paul.elliott@kitware.com>
2023-12-31 10:21:54 -0500

    feat(client): bump version for segment group conversion features

--- volview-girder-client/buildvolview.sh
+++ volview-girder-client/buildvolview.sh
@@ -6,14 +6,14 @@ cd VolView
 # fetch just one commit
 git init
 git remote add origin https://github.com/Kitware/VolView.git
-git fetch origin 7db5959dfda934b06182c74ef0c3a8552aa7f2f7 --depth 1
+git fetch origin b1951c47250763068793e58e120bc3e6f23daadc --depth 1
 git reset --hard FETCH_HEAD
 
 npm install
 npm run postinstall # avoid starting the build before patch-package done by running postinstall manualy 
 VITE_REMOTE_SERVER_URL= VITE_ENABLE_REMOTE_SAVE=true npm run build -- --base=/static/built/plugins/volview
 
-# remote so npm publish picks up VolView/dist
+# remove so npm publish picks up VolView/dist
 rm .gitignore
 
 cd ..
