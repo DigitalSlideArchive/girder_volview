c2c9341bdaf3ddf1b2a49b213f83daeb8ebfa87a
Paul Elliott <paul.elliott@kitware.com>
2024-02-26 20:08:24 -0500

    feat(HierarchyWidget): change button message if something is checked

--- girder_volview/web_client/views/HierarchyWidget.js
+++ girder_volview/web_client/views/HierarchyWidget.js
@@ -1,16 +1,28 @@
 import HierarchyWidget from "@girder/core/views/widgets/HierarchyWidget";
 import { wrap } from "@girder/core/utilities/PluginUtils";
 import { openButton, openResources } from "./open";
-import { restRequest } from "@girder/core/rest";
+
+const openFolder = '<i class="icon-link-ext"></i>Open Folder in VolView</a>';
+const openChecked = '<i class="icon-link-ext"></i>Open Checked in VolView</a>';
 
 wrap(HierarchyWidget, "render", function (render) {
     render.call(this);
 
     this.$(".g-folder-header-buttons").prepend(openButton);
     const buttons = this.$el.find(".open-in-volview");
+    const button = buttons[0];
 
-    buttons[0].onclick = () => {
+    button.onclick = () => {
         const resources = JSON.parse(this._getCheckedResourceParam());
         openResources(this.parentModel, resources);
     };
+
+    const updateChecked = () => {
+        const resources = this._getCheckedResourceParam();
+        button.innerHTML = resources.length >= 3 ? openChecked : openFolder;
+    };
+    updateChecked();
+
+    this.listenTo(this.itemListView, "g:checkboxesChanged", updateChecked);
+    this.listenTo(this.folderListView, "g:checkboxesChanged", updateChecked);
 });
