cbd1d0d8eb96a12b1dce2e86b921a72cd73d11d1
Paul Elliott <paul.elliott@kitware.com>
2024-04-29 21:32:10 -0400

    feat(client): bump version for auto-seg

--- volview-girder-client/buildvolview.sh
+++ volview-girder-client/buildvolview.sh
@@ -6,7 +6,7 @@ cd VolView
 # fetch just one commit
 git init
 git remote add origin https://github.com/Kitware/VolView.git
-git fetch origin 3d63d5e28d494f553a545bfde73ccef09c988313 --depth 1
+git fetch origin 35fa47f7388f8569480341fc12b8b6f87c78e1ee --depth 1
 git reset --hard FETCH_HEAD
 
 npm install
