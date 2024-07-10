cf04c4a5cb14ec246a7b88a536fec36b46fbbe58
Paul Elliott <paul.elliott@kitware.com>
2024-07-10 11:39:49 -0400

    feat(client): bump for merge polygon feature

--- volview-girder-client/buildvolview.sh
+++ volview-girder-client/buildvolview.sh
@@ -6,7 +6,7 @@ cd VolView
 # fetch just one commit
 git init
 git remote add origin https://github.com/Kitware/VolView.git
-git fetch origin f25fb04c293e0df23ca5ecc05f13d5a688e91e36 --depth 1
+git fetch origin 72bf66b5c2df88b1e5d17f50632f545fc760ceb1 --depth 1
 git reset --hard FETCH_HEAD
 
 npm install
