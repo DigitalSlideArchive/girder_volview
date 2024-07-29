fb349b0972ebed371b66181143ec6369d78a280d
David Manthey <david.manthey@kitware.com>
2024-07-29 15:53:10 -0400

    Only show the Open in Volview button for some file extensions.
    This will only show the Open in Volview button for item lists if at
    least one item has a final extension that matches a list.  For item
    pages, the button will only show if the item has such an extension.
    
    This is still imperfect -- item extensions are not required, and the
    extensions are the wrong granularity (e.g., .gz is too inclusive).  This
    is still an improvement over the current always-show-button behavior.
    
    In the future, we can tune the extensions or add mime-type detection as
    well (or even check some other configuration).

--- girder_volview/web_client/views/itemPage.js
+++ girder_volview/web_client/views/itemPage.js
@@ -1,11 +1,16 @@
 import { wrap } from "@girder/core/utilities/PluginUtils";
 import ItemView from "@girder/core/views/body/ItemView";
-import { openItem, addButton } from "./open";
+import { openItem, addButton, knownExtensions } from "./open";
 
 wrap(ItemView, "render", function (render) {
     this.once("g:rendered", function () {
+        const lastExt = this.model.get('name').split('.').slice(-1)[0].toLowerCase();
+        if (!knownExtensions.includes(lastExt)) {
+            return;
+        }
         const button = addButton(this.$el, ".g-item-header .btn-group");
         if (button) {
+            this.$el.find('.open-in-volview').removeClass('hidden');
             button.onclick = () => openItem(this.model);
         }
     });
