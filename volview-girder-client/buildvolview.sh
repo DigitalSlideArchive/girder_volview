c9b5117c65ab06b7c5969642bf62fc4353d84b5b
Paul Elliott <paul.elliott@kitware.com>
2024-12-19 14:15:34 -0500

    feat(client): bump version for new ct presets, etc

--- volview-girder-client/buildvolview.sh
+++ volview-girder-client/buildvolview.sh
@@ -6,7 +6,7 @@ cd VolView
 # fetch just one commit
 git init
 git remote add origin https://github.com/Kitware/VolView.git
-git fetch origin 9d5eeab6ade5770f7838ea0cebdf185d34dd4612 --depth 1
+git fetch origin a76e8fb308ab71efd0dd92568957fff72c053a2e --depth 1
 git reset --hard FETCH_HEAD
 
 npm install
