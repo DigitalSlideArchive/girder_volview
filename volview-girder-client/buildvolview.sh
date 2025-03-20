d5472824fa3520e82d27b88094020414f972aa14
Paul Elliott <paul.elliott@kitware.com>
2025-03-20 14:09:59 -0400

    feat(client): include probe, batch seg sliders, etc.

--- volview-girder-client/buildvolview.sh
+++ volview-girder-client/buildvolview.sh
@@ -5,13 +5,13 @@ cd VolView
 
 # fetch just one commit
 git init
-git remote add origin https://github.com/Kitware/VolView.git
-git fetch origin a76e8fb308ab71efd0dd92568957fff72c053a2e --depth 1
+git remote add origin https://github.com/PaulHax/VolView.git
+git fetch origin fix-scalar-prob --depth 1
 git reset --hard FETCH_HEAD
 
 npm install
-npm run postinstall # avoid starting the build before patch-package done by running postinstall manualy 
-VITE_REMOTE_SERVER_URL= VITE_ENABLE_REMOTE_SAVE=true npm run build -- --base=/static/built/plugins/volview
+npm run postinstall # avoid starting the build before patch-package is finished by running postinstall explicitly 
+VITE_REMOTE_SERVER_URL= VITE_ENABLE_REMOTE_SAVE=true npm run build
 
 # remove so npm publish picks up VolView/dist
 rm .gitignore
